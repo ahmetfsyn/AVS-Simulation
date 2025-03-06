b14ec1faf4fd85def895d45a7cce95a4cc2e79e3
Ahmet Furkan Sayan <21010310065@ogrenci.bartin.edu.tr>
2025-03-06 12:48:26 +0300

    unnecessary

--- mobile/AvsMobile/App.tsx
+++ mobile/AvsMobile/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import Home from './src/pages/Home';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App(): React.JSX.Element {
-  return <Home></Home>;
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
